a7101267514bda41361127b4cf32a600f41ab7f1
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2022-04-03 09:57:37 +0200

    Play sound 20, and 10 seconds before

--- web/src/App.js
+++ web/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Timer from './Timer';
 import { useEffect, useState } from 'react';
+import alarm from "./static/alarm.wav";
 
 const DURATION = 120;
 
 const App = () => {
-  const [seconds, setSeconds] = useState(DURATION)
-  const [start, setStart] = useState()
+  const [seconds, setSeconds] = useState(DURATION);
+  const [start, setStart] = useState();
+  const [audio] = useState(new Audio(alarm));
 
   useEffect(() => {
     const id = setInterval(() => {
@@ -16,16 +18,31 @@ const App = () => {
 
       const secondsPassed = Math.floor((Date.now() - start) / 1000);
       setSeconds(DURATION - (secondsPassed % DURATION) - 1);
-    }, 300);
+    }, 100);
     return () => clearInterval(id);
   }, [start]);
 
+  useEffect(() => {
+    if (seconds === 20 || seconds === 10) {
+      audio.play();
+    }
+  }, [seconds]);
+
   return (<div className="App">
+    <h1>Tibia fishes tracker</h1>
+    <div>
+      <button onClick={() => {
+        setSeconds(DURATION);
+        setStart(Date.now());
+      }}>start</button>
+
+      <button onClick={() => {
+        setStart(null);
+        setSeconds(DURATION);
+      }}>stop</button>
+    </div>
+
     <Timer seconds={seconds}/>
-    <button onClick={() => {
-      setSeconds(DURATION)
-      setStart(Date.now())
-    }}>start</button>
   </div>);
 };
 
--- web/src/Timer.jsx
+++ web/src/Timer.jsx
@@ -1,5 +1,4 @@
-const Timer = ({seconds}) => (
-    <h1>{Math.floor(seconds/60)}:{String(seconds%60).padStart(2, '0')}</h1>
-);
+const Timer = ({seconds}) =>
+    (<div className="Timer">{Math.floor(seconds/60)}:{String(seconds%60).padStart(2, '0')}</div>)
 
 export default Timer;
