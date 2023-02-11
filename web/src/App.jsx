850c9b98fb7bc3dc0649c2c692a1f89f775c5597
Jakub Podeszwik <jakubpodeszwik@gmail.com>
2023-02-11 14:08:53 +0100

    Optimize loading sounds, store volume value in localstorage

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -10,12 +10,51 @@ import VolumeBar from './VolumeBar';
 
 const DURATION = 120;
 
+const useAudio = url => {
+  const [audio] = useState(() => new Audio(url));
+  const [playing, setPlaying] = useState(false);
+
+  useEffect(() => {
+    console.log('useEffect');
+    audio.currentTime = 0;
+    playing ? audio.play() : audio.pause();
+  }, [playing, audio]);
+
+  useEffect(() => {
+    audio.addEventListener('ended', () => setPlaying(false));
+    return () => {
+      audio.removeEventListener('ended', () => setPlaying(false));
+    };
+  }, [audio]);
+
+  return {
+    play: () => setPlaying(true),
+    stop: () => setPlaying(false),
+    setVolume: volume => audio.volume = volume,
+  };
+};
+
+const useAudios = urls => {
+  const audios = urls.map(useAudio);
+
+  return {
+    play: num => {
+      audios.forEach(a => a.stop());
+      audios[num].play();
+    },
+    setVolume: volume => audios.forEach(a => a.setVolume(volume)),
+  }
+};
+
 const App = () => {
-  const [seconds, setSeconds] = useState(DURATION);
+  const audios = useAudios([alarm1, alarm2, alarm3]);
+  const [secondsElapsed, setSecondsElapsed] = useState();
   const [start, setStart] = useState();
-  const [audio] = useState([new Audio(alarm1), new Audio(alarm2), new Audio(alarm3)]);
-  const [water, setWater] = useState(true);
-  const [volume, setVolume] = useState(50);
+  const [volume, setVolume] = useState();
+
+  const cyclesPassed = secondsElapsed ? Math.floor(secondsElapsed / DURATION) : 0;
+  const water = cyclesPassed % 2 === 0;
+  const seconds = secondsElapsed ? DURATION - (secondsElapsed % DURATION) - 1 : DURATION;
 
   useEffect(() => {
     /* eslint no-restricted-globals: "off" */
@@ -26,39 +65,34 @@ const App = () => {
   }, []);
 
   useEffect(() => {
+    if (!start) {
+      return;
+    }
     const id = setInterval(() => {
-      if (!start) {
-        return;
-      }
-      const secondsPassed = Math.floor((Date.now() - start) / 1000);
-      const cyclesPassed = Math.floor(secondsPassed / DURATION);
-      setWater(cyclesPassed % 2 == 0);
-      setSeconds(DURATION - (secondsPassed % DURATION) - 1);
+      setSecondsElapsed(Math.floor((Date.now() - start) / 1000));
     }, 100);
     return () => clearInterval(id);
   }, [start]);
 
   useEffect(() => {
     if (seconds === 20) {
-      audio[2].play();
+      audios.play(2);
     }
-    if(seconds === 10) {
-      audio[1].play();
+    if (seconds === 10) {
+      audios.play(1);
     }
-    if(seconds === 0) {
-      audio[0].play();
+    if (seconds === 0) {
+      audios.play(0);
     }
-  }, [seconds, audio]);
+  }, [seconds, audios]);
 
   useEffect(() => {
-    const newVolume = volume / 100;
-    audio[0].volume = newVolume;
-    audio[1].volume = newVolume;
-    audio[2].volume = newVolume;
-  }, [volume, audio]);
+    if (volume) {
+      audios.setVolume(volume / 100);
+    }
+  }, [volume, audios]);
 
   const startTimer = () => {
-    setSeconds(DURATION);
     const newStart = Date.now() - 10 * DURATION * 1000;
     setStart(newStart);
     /* eslint no-restricted-globals: "off" */
@@ -67,17 +101,19 @@ const App = () => {
 
   const stopTimer = () => {
     setStart(null);
-    setSeconds(DURATION);
+    /* eslint no-restricted-globals: "off" */
+    parent.location.hash = '';
   };
 
   const addSeconds = (seconds) => {
     const newStart = start + (seconds * 1000);
     setStart(newStart);
+    /* eslint no-restricted-globals: "off" */
     parent.location.hash = newStart;
   };
 
   return (<div className="App">
-    <Header/>
+    <Header />
     <div className="button-bar">
       <button disabled={!!start} onClick={startTimer}>start</button>
       <button disabled={!start} onClick={stopTimer}>stop</button>
@@ -86,8 +122,8 @@ const App = () => {
       <button disabled={!start} onClick={() => addSeconds(-120)}>-120s</button>
     </div>
     <VolumeBar onChange={setVolume} />
-    <Timer seconds={seconds}/>
-    {start && <WaterStatus status={water}/>}
+    <Timer seconds={seconds} />
+    {start && <WaterStatus status={water} />}
   </div>);
 };
 
--- web/src/VolumeBar.jsx
+++ web/src/VolumeBar.jsx
@@ -1,22 +1,36 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 
-const VolumeBar = ({onChange}) => {
-    const [value, setValue] = useState(50);
+const useLocalStorage = (defaultValue, key) => {
+    const [value, setValue] = useState(defaultValue);
 
-    const valueChanged = val => {
-        setValue(val);
-        if (onChange) {
-            onChange(val);
+    useEffect(() => {
+        const storageVal = Number.parseInt(localStorage.getItem(key), 10);
+        if (!Number.isNaN(storageVal)) {
+            setValue(storageVal);
         }
-    }
+    }, [key]);
+
+    return [value, val => {
+        localStorage.setItem(key, val);
+        setValue(val);
+    }];
+};
+
+const VolumeBar = ({ onChange }) => {
+    const [value, setValue] = useLocalStorage(20, 'volume');
+
+    useEffect(() => {
+        if (onChange) {
+            onChange(value);
+        }
+    }, [value, onChange]);
 
     return (
         <div className="volume-bar">
             <span>Volume {value}</span>
-            <input type="range" min="0" max="100" value={value} onChange={e => valueChanged(e.target.value)} ></input>
+            <input type="range" min="0" max="100" value={value} onChange={e => setValue(e.target.value)} ></input>
         </div>
     );
-
 };
 
 export default VolumeBar;
